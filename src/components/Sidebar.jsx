1310ebb21c04c972760320daded91883d10239db
Shintaro Kawauchi <kawauchi0tech@gmail.com>
2020-08-16 09:29:36 +0900

    fixed Sidebar to link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
 import { Sidenav, Nav, Icon } from 'rsuite';
 
 const Sidebar = () => {
+
+  const history = useHistory();
+  const [activeKey, setActiveKey] = useState('1');
+
+  const handleSelect = (eventKey) => {
+    setActiveKey(eventKey);
+  };
+
+  const toLink = (to) => {
+    history.push(to);
+  };
+
   return (
-    <Sidenav style={{ width: 250, height: 'calc(100vh - 56px)' }}>
+    <Sidenav activeKey={activeKey} onSelect={handleSelect} style={{ width: 250, height: 'calc(100vh - 56px)' }}>
       <Sidenav.Body>
         <Nav>
-          <Nav.Item eventKey="1" icon={<Icon icon='dashboard' size='3x' />}>
-            <Link to="/">Dashboard</Link>
+          <Nav.Item eventKey='1' icon={<Icon icon='dashboard' size='3x' />} onClick={() => toLink('/')}>
+            Dashboard
           </Nav.Item>
-          <Nav.Item eventKey="2" icon={<Icon icon='user' size='5x' />}>
-            <Link to="/members">Members</Link>
+          <Nav.Item eventKey='2' icon={<Icon icon='user' size='5x' />} onClick={() => toLink('/members')}>
+            Members
           </Nav.Item>
-          <Nav.Item eventKey="3" icon={<Icon icon='gear-circle' size='5x' />}>
-            <Link to="/config">Config</Link>
+          <Nav.Item eventKey='3' icon={<Icon icon='gear-circle' size='5x' />} onClick={() => toLink('/config')}>
+            Config
           </Nav.Item>
         </Nav>
       </Sidenav.Body>
