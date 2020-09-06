5964b7dff2f549165d92215e5fd995a2211c1bab
Shintaro Kawauchi <kawauchi0tech@gmail.com>
2020-09-06 17:43:46 +0900

    used Context Api

--- src/pages/Records/index.jsx
+++ src/pages/Records/index.jsx
@@ -2,15 +2,62 @@ import React from 'react';
 
 import Divider from '../../components/Divider';
 import RecordTable from './widget/RecordTable';
+import { Provider } from './hoc/index';
 
+const value = {
+  data: [
+    {
+      id: 1,
+      title: 'ひき肉',
+      category: '食品',
+      date: '2020/08/31',
+      member: 'shin',
+      cost: 450
+    },
+    {
+      id: 2,
+      title: 'アイス',
+      category: '食品',
+      date: '2020/08/31',
+      member: 'mari',
+      cost: 2000
+    },
+    {
+      id: 3,
+      title: '電気',
+      category: '電気代',
+      date: '2020/08/31',
+      member: 'shin',
+      cost: 7500
+    },
+    {
+      id: 4,
+      title: 'ガス',
+      category: 'ガス代',
+      date: '2020/08/31',
+      member: 'mari',
+      cost: 5500
+    },
+    {
+      id: 5,
+      title: 'トイレットペーパー',
+      category: '生活用品',
+      date: '2020/08/31',
+      member: 'mari',
+      cost: 500
+    }
+  ]
+};
 
 const Records = () => {
   return (
     <div className='wrap'>
-      <h2 >Records</h2>
-      <Divider height='20' />
-      <RecordTable />
-      <Divider height='20' />
+      <Provider value={value}>
+        <h2 >Records</h2>
+        <Divider height='20' />
+        <RecordTable />
+        <Divider height='20' />
+      </Provider>
     </div>
   );
 };
--- src/pages/Records/widget/RecordTable.jsx
+++ src/pages/Records/widget/RecordTable.jsx
@@ -4,54 +4,11 @@ import { Row, Col, Table, Panel, Button } from 'rsuite';
 import Divider from '../../../components/Divider';
 import SectionTitle from '../../../components/SectionTitle';
 import AddModal from '../widget/AddModal';
+import { withRecord } from '../hoc/index';
 
 const { Column, HeaderCell, Cell } = Table;
-
-const data = [
-  {
-    id: 1,
-    title: 'ひき肉',
-    category: '食品',
-    date: '2020/08/31',
-    member: 'shin',
-    cost: 450
-  },
-  {
-    id: 2,
-    title: 'アイス',
-    category: '食品',
-    date: '2020/08/31',
-    member: 'mari',
-    cost: 2000
-  },
-  {
-    id: 3,
-    title: '電気',
-    category: '電気代',
-    date: '2020/08/31',
-    member: 'shin',
-    cost: 7500
-  },
-  {
-    id: 4,
-    title: 'ガス',
-    category: 'ガス代',
-    date: '2020/08/31',
-    member: 'mari',
-    cost: 5500
-  },
-  {
-    id: 5,
-    title: 'トイレットペーパー',
-    category: '生活用品',
-    date: '2020/08/31',
-    member: 'mari',
-    cost: 500
-  }
-];
-
-const RecordTable = () => {
-
+const RecordTable = (props) => {
+  const { data } = props;
   const [show, setShow] = useState(false);
   const openAddModal = () => {
     setShow(true);
@@ -113,4 +70,4 @@ const RecordTable = () => {
   );
 };
 
-export default RecordTable;
+export default withRecord(RecordTable);
