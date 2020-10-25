f9c3c60e6d566fc80ee85da8baa9e95034de16c6
Shintaro Kawauchi <kawauchi0tech@gmail.com>
2020-10-25 09:25:00 +0900

    improved summary table

--- src/components/Units/index.jsx
+++ src/components/Units/index.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 
 const yen = (style) => ({
   marginLeft: 6,
-  fontSize: 'inherit',
-  ...style
+  ...style,
 });
 
-export const YenUnit = (style) => {
+export const YenUnit = ({ style }) => {
   return <span style={yen(style)}>円</span>;
 };
