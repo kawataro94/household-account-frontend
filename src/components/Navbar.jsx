9511570f7aec8aae3fd6348993da6217ad381bcb
kawataro94 <kawauchi0tech@gmail.com>
2021-01-23 21:58:22 +0900

    signout (#26)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useHistory, useLocation } from "react-router-dom";
 import { Navbar, Nav, Icon } from 'rsuite';
+import Axios from 'axios';
+
+import { serverUrl } from '../../.env/resources';
 
 const navStyle = {
   padding: '18px 20px',
@@ -9,6 +12,10 @@ const navStyle = {
   fontWeight: 'bold'
 };
 
+const httpClient = Axios.create({
+  withCredentials: true
+});
+
 const Header = () => {
   const history = useHistory();
   const { pathname } = useLocation();
@@ -16,14 +23,23 @@ const Header = () => {
     history.push(to);
   };
 
+  const signOut = () => {
+    httpClient
+      .post(`http://${serverUrl}/member/signout`)
+      .then(() => {
+        toLink('/signin');
+      });
+  };
+
   return (
     <Navbar appearance="subtle">
       <Navbar.Header>
         <a href="#" style={navStyle} >Household Account</a>
       </Navbar.Header>
-      <Nav pullRight activeKey={pathname} className='header-link'>
-        <Nav.Item eventKey='/' icon={<Icon icon="dashboard" size="2x" />} onClick={() => toLink('/')} ></Nav.Item>
-        <Nav.Item eventKey='/records' icon={<Icon icon="table" size="2x" />} onClick={() => toLink('/records')}></Nav.Item>
+      <Nav pullRight activeKey={pathname} >
+        <Nav.Item className='mobile-menu' eventKey='/' icon={<Icon icon="dashboard" size="2x" />} onClick={() => toLink('/')} ></Nav.Item>
+        <Nav.Item className='mobile-menu' eventKey='/records' icon={<Icon icon="table" size="2x" />} onClick={() => toLink('/records')}></Nav.Item>
+        <Nav.Item icon={<Icon icon="sign-out" size="2x" />} onClick={() => signOut()}></Nav.Item>
       </Nav>
     </Navbar>
   );
