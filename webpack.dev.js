6892326af7e6e6a1cee411258a7d7a5e3d3ae4c7
Shintaro Kawauchi <kawauchi0tech@gmail.com>
2021-02-23 15:23:22 +0900

    added webpack common config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,91 +1,15 @@
 const path = require('path');
 const webpack = require('webpack');
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-
-module.exports = {
-  entry: './src/index.js',
+module.exports = merge(common, {
   mode: 'development',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: ''
-  },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Household-Accounts',
-      hash: true,
-    }),
     new webpack.DefinePlugin({
       'process.env.RUN_ENV': JSON.stringify('development')
     })
   ],
-  optimization: {
-    minimizer: [
-      new TerserPlugin({}),
-      new OptimizeCSSAssetsPlugin({})
-    ],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|mjs|jsx)$/,
-        enforce: 'pre',
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'eslint-loader',
-            options: {
-              fix: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(scss|sass|less|css)$/i,
-        use: [
-          MiniCssExtractPlugin.loader, 'css-loader'
-        ],
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: './images/[name].[ext]'
-            }
-          }
-        ],
-      },
-      {
-        test: /\.(js|mjs|jsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                '@babel/preset-env',
-                '@babel/preset-react',
-                "@emotion/babel-preset-css-prop"
-              ],
-              plugins: ["transform-class-properties"]
-            }
-          }
-        ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     port: 3000,
@@ -94,4 +18,4 @@ module.exports = {
     historyApiFallback: true
   },
   devtool: 'inline-source-map'
-};
+});
