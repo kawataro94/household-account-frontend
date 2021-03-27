13f4c64e57fb81a55345d39f4b60f87550ce749a
kawataro94 <kawauchi0tech@gmail.com>
2021-03-27 23:58:39 +0900

    service worker precache (#46)
    * add: service worker
    * add: precache

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,21 +1,45 @@
 const path = require('path');
 const webpack = require('webpack');
-const { merge } = require('webpack-merge')
-const common = require('./webpack.common.js')
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
 
 module.exports = merge(common, {
-  mode: 'development',
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.RUN_ENV': JSON.stringify('development')
-    })
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    port: 3000,
-    open: true,
-    compress: true,
-    historyApiFallback: true
-  },
-  devtool: 'inline-source-map'
+	mode: 'development',
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env.RUN_ENV': JSON.stringify('development'),
+		}),
+        new HtmlWebpackPlugin({
+			title: 'Household-Accounts DEV',
+			template: path.resolve(__dirname, 'template.ejs'),
+        }),
+		new GenerateSW({
+			clientsClaim: true,
+			skipWaiting: true,
+			maximumFileSizeToCacheInBytes: 1024 * 1024 * 11,
+			runtimeCaching: [
+				{
+					urlPattern: /.+(\/|.html)$/,
+					handler: 'NetworkFirst',
+					options: {
+						cacheName: 'household-accounts' + '-html-cache',
+						expiration: {
+							maxAgeSeconds: 60 * 60 * 24 * 7,
+						},
+					},
+				},
+			],
+		}),
+	],
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		port: 3000,
+		open: true,
+		compress: true,
+		historyApiFallback: true,
+	},
+	devtool: 'inline-source-map',
 });
