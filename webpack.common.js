a04cda11a5eb3bab96d1cb0a7bff7a51a77dbfc8
Shintaro Kawauchi <kawauchi0tech@gmail.com>
2021-03-27 15:33:07 +0900

    add: service worker

--- webpack.common.js
+++ webpack.common.js
@@ -1,85 +1,70 @@
 const path = require('path');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: ''
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Household-Accounts',
-      hash: true,
-    })
-  ],
-  optimization: {
-    minimizer: [
-      new TerserPlugin({}),
-      new OptimizeCSSAssetsPlugin({})
-    ],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|mjs|jsx)$/,
-        enforce: 'pre',
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'eslint-loader',
-            options: {
-              fix: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(scss|sass|less|css)$/i,
-        use: [
-          MiniCssExtractPlugin.loader, 'css-loader'
-        ],
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: './images/[name].[ext]'
-            }
-          }
-        ],
-      },
-      {
-        test: /\.(js|mjs|jsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                '@babel/preset-env',
-                '@babel/preset-react',
-                "@emotion/babel-preset-css-prop"
-              ],
-              plugins: ["transform-class-properties"]
-            }
-          }
-        ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  }
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '',
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'style.css',
+		}),
+	],
+	optimization: {
+		minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|mjs|jsx)$/,
+				enforce: 'pre',
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'eslint-loader',
+						options: {
+							fix: true,
+						},
+					},
+				],
+			},
+			{
+				test: /\.(scss|sass|less|css)$/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader'],
+			},
+			{
+				test: /\.(gif|png|jpe?g|svg)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: './images/[name].[ext]',
+						},
+					},
+				],
+			},
+			{
+				test: /\.(js|mjs|jsx)$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env', '@babel/preset-react', '@emotion/babel-preset-css-prop'],
+							plugins: ['transform-class-properties'],
+						},
+					},
+				],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,38 @@
+const path = require('path');
 const webpack = require('webpack');
-const { merge } = require('webpack-merge')
-const common = require('./webpack.common.js')
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
 
 module.exports = merge(common, {
-  mode: 'production',
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.RUN_ENV': JSON.stringify('production')
-    })
-  ]
+	mode: 'production',
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env.RUN_ENV': JSON.stringify('production'),
+		}),
+		new HtmlWebpackPlugin({
+			title: 'Household-Accounts Prod',
+			template: path.resolve(__dirname, 'template.ejs'),
+			hash: true,
+		}),
+		new GenerateSW({
+			clientsClaim: true,
+			skipWaiting: true,
+			maximumFileSizeToCacheInBytes: 1024 * 1024 * 11,
+			runtimeCaching: [
+				{
+					urlPattern: /.+(\/|.html)$/,
+					handler: 'NetworkFirst',
+					options: {
+						cacheName: 'household-accounts' + '-html-cache',
+						expiration: {
+							maxAgeSeconds: 60 * 60 * 24 * 7,
+						},
+					},
+				},
+			],
+		}),
+	],
 });
